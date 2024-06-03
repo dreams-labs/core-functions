9e0bc74d2a929620c8428d24d9362272a414ec54
tentabs00 <tentabs00@gmail.com>
2024-06-03 16:25:51 -0700

    update tests to align with code updates

--- tests/core/test_human_format.py
+++ tests/core/test_human_format.py
@@ -2,7 +2,7 @@ import src.dreams_core.core
 
 def test_human_format_zero():
     """ Test the human_format function with 0 as input. """
-    assert src.dreams_core.core.human_format(0) == '0'
+    assert src.dreams_core.core.human_format(0) == '0.0'
     
 def test_human_format_decimal():
     """ Test the human_format function with a decimal number as input. """
